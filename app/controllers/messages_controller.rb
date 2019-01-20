189056633e4b3284e82cda166dcb9e1d0a7c240b
yukitohiroki <qfpng09604@yahoo.co.jp>
2019-01-20 20:33:18 +0900

    edit index message bottom
    指摘箇所の修正のため

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_group,only: [:index, :create]
+  before_action :set_group, only: [:index, :create]
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
